1b103bd6594a9157c24601c6ec7ac28411347329
OmarLeal2503 <omarenriquelealr@gmail.com>
2021-02-06 11:41:01 -0400

    Setup Finished

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import HelloWorld from './components/HelloWorld'
+
+const container = document.getElementById('container')
+
+ReactDOM.render(<HelloWorld />, container)
