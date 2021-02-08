a9e2dce35295d59f1ff78889b20b333deb46a637
OmarLeal2503 <omarenriquelealr@gmail.com>
2021-02-08 01:20:08 -0400

    Finished first part

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,53 +1,61 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Searcher from '../components/Searcher'
 import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import Item from '../components/Item'
 import Footer from '../components/Footer'
+import useInitialState from '../Hooks/useInitialState'
+
+const API = 'http://localhost:3000/initalState'
 
 import '../assets/styles/App.scss'
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Searcher />
-    <Categories title="My list">
-                              <Carousel>
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                              </Carousel>
-    </Categories>
+const App = () => {
+ const  videos = useInitialState(API);
 
-    <Categories title="Trends">
-                              <Carousel>
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                              </Carousel>
-    </Categories>
+   const renderList = (video = []) => {
+     return(
+        <>
+        {video.map((item) =>(
+          <Item key={item.id} {...item} />
+        ))}
+        </>
+     )
+   }     
+  
+    return videos.length === 0 ? <h1>Loading...</h1> : (
+    <div className="App">
+      <Header />
+      <Searcher />
+        {videos.mylist.length > 0 &&
+            <Categories title="My list">
+              <Carousel>
+                <Item />                    
+              </Carousel>
+            </Categories>    
+        }
 
-    <Categories title="Original from Platzi Videos">
-                              <Carousel>
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                                        <Item />
-                              </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+      <Categories title="Trends">
+        <Carousel>
+          {renderList(videos.trends)}
+
+        </Carousel>
+      </Categories>
+
+      <Categories title="Originals">
+        <Carousel>
+          {renderList(videos.originals)}
+        </Carousel>
+      </Categories>
+
+      
+      <Footer />
+    </div>
+  )
+
+
+};
 
 
 export default App;
