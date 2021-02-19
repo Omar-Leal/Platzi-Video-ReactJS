e6797b23f04ce70e9402cfe90d6420e5774919d5
OmarLeal2503 <omarenriquelealr@gmail.com>
2021-02-18 21:02:29 -0400

    Some Links added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import '../assets/styles/components/Header.scss'
 import logo from '../assets/static/logo-platzi-video-BW2.png'
 import user from '../assets/static/user-icon.png'
@@ -7,15 +7,17 @@ import user from '../assets/static/user-icon.png'
 const Header = () => (
   
   <header className="header">
+    <Link to='/'>
         <img className="header__img" src={logo} alt="Platzi Video" />
+    </Link>
     <div className="header__menu">
         <div className="header__menu--profile">
           <img src={user} alt="user__foto" />
           <p>Perfil</p>
         </div>
         <ul>
-          <li><a href="/">Cuenta</a></li>
-          <li><a href="/">Cerrar Sesión</a></li>
+          <li><Link to='/'>Iniciar Sesión</Link></li>
+          <li><Link to='/'>Cerrar Sesión</Link></li>
         </ul>
     </div>
   </header>
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 import googleIcon from '../assets/static/google-icon.png'
 import twitterIcon from '../assets/static/twitter-icon.png'
@@ -17,14 +19,18 @@ const Login = () => (
             <label>
               <input type="checkbox" id="cbox1" value="first_checkbox" />Recuérdame
             </label>
-            <a href="/">Olvidé mi contraseña</a>
+            <Link to='/'>Olvidé mi contraseña</Link>
           </div>
         </form>
         <section className="login__container--social-media">
                 <div><img src={googleIcon} /> Inicia sesión con Google</div>
                 <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
         </section>
-        <p className="login__container--register">No tienes ninguna cuenta <a href="">Regístrate</a></p>
+        <p className="login__container--register">No tienes ninguna cuenta 
+        <Link to='/register'>
+          Registrate
+        </Link>        
+        </p>
       </section>
   </section>
 
